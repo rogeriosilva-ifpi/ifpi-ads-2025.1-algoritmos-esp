6e1dd6560ac8d5562bd23587517353249c4efd4c
Rogério Silva <rogerio410@gmail.com>
2025-07-09 15:03:52 -0300

    Sistema Series

--- m9-colecoes/series/sys_series.py
+++ m9-colecoes/series/sys_series.py
@@ -20,6 +20,8 @@ def main():
   9 - Pesquisa por Gênero
   10 - Contar por Gênero
   11 - Listar por Avaliação Min-Max
+  12 - Listar Ordenado por Qualquer Atributo
+  13 - Listar Filtras por Qualquer Atributo
 
   0 - Sair
   Opcão >>> '''
@@ -41,6 +43,24 @@ def main():
       lista = list(map(formatar_ha_x_anos, series))
       for item in lista:
         print(item)
+    elif opcao == 5:
+      series_ativas = obter_series_ativas(series)
+      listar_series(series_ativas)
+    elif opcao == 7:
+      # series_ordenadas = sorted(series, key=por_ano_lancamento, reverse=True)
+      series_ordenadas = sorted(series, key=lambda serie:serie['ano'], reverse=True)
+      listar_series(series_ordenadas)
+    elif opcao == 12:
+      atributo = input('Atributo: ')
+      ordem = input('DESC ou Enter para ASC: ')
+      valor_reverse = False
+      if ordem == 'DESC':
+        valor_reverse = True
+
+      series_ordenadas = sorted(series, 
+                                key=lambda serie:serie[atributo], 
+                                reverse=valor_reverse)
+      listar_series(series_ordenadas)
 
     continuar()
     limpar_tela()
@@ -50,7 +70,13 @@ def main():
   salvar_em_arquivo(series, 'series.txt')
 
 # Funcionalidades
-def carregar_arquivo(nome_arquivo):
+
+# funcao criterio de ordenacao
+def por_ano_lancamento(serie: dict):
+  return serie['ano']
+
+
+def carregar_arquivo(nome_arquivo: str):
   arquivo = open(nome_arquivo)
   conteudo = arquivo.read()
 
@@ -62,13 +88,14 @@ def carregar_arquivo(nome_arquivo):
   print(f' {len(series)} séries carregadas com sucesso!')
   return series
 
-
-def salvar_em_arquivo(series, nome_arquivo):
-  arquivo = open(nome_arquivo, 'w')
+# TypeHint
+def salvar_em_arquivo(series: list[dict], nome_arquivo: str):
   conteudo = json.dumps(series)
-  arquivo.write(conteudo)
 
+  arquivo = open(nome_arquivo, 'w')
+  arquivo.write(conteudo)
   arquivo.close()
+
   print(f' {len(series)} séries salvas com sucesso!')
 
 
@@ -84,6 +111,9 @@ def obter_nova_serie():
   serie['ano'] = int(input('Ano de Lançamento: '))
   serie['genero'] = input('Genero: ')
   serie['avaliacao'] = float(input('Avalição[0.0 a 5.0]: '))
+  
+  ativa = input('Série Ativa? S ou N: ')
+  serie['ativa'] = True if ativa == 'S' else False # Ternário
 
   return serie
 
@@ -92,11 +122,20 @@ def listar_series(series:list[dict]):
   print(f'Listando {len(series)} séries:')
 
   for serie in series:
-    resultado = f'{serie['id']} > {serie['nome']} ({serie['ano']}) - G: {serie['genero']} - Nota: {serie['avaliacao']:.1f}'
+    resultado = f'{serie['id']} > {serie['nome']} ({serie['ano']}) - G: {serie['genero']} - Nota: {serie['avaliacao']:.1f} - Ativa: {'SIM' if serie['ativa'] == True else 'NÃO'}'
     print(resultado)
 
   print('>>>>>>>> Fim <<<<<<<<<')
 
+
+def obter_series_ativas(series):
+  cesta = []
+
+  for serie in series:
+    if serie['ativa'] >= True:
+      cesta.append(serie)
+
+  return cesta
 
 def filtrar_por_periodo(series, inicio, fim):
   cesta = []
